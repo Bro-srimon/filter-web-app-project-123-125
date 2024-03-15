9deb2c8a02e223ceb80ea27d1f586dbb6baf73be
Bro-srimon <139974774+Bro-srimon@users.noreply.github.com>
2024-03-15 11:20:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.color = "grey";
+ctx.width = 1;
+ctx.rect(150, 143, 430, 200);
+ctx.stroke();
+
+ctx.beginPath;
+ctx.color = "blue";
+ctx.width = 5;
+ctx.arc(250, 120, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath;
+ctx.color = "black";
+ctx.width = 5;
+ctx.arc(350, 210, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath;
+ctx.color = "red";
+ctx.width = 5;
+ctx.arc(210, 40, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+etx.beginPath;
+ctx.color = "orange";
+ctx.width = 5;
+ctx.arc(300, 250, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath;
+ctx.color = "blue";
+ctx.width = 5;
+ctx.arc(400, 250, 40, 0, 2 * Math.PI);
+ctx.stroke();
